697ed999b9bb68122d8f98e32f158ad911756791
Chris.Hettinger <chrishettinger1@gmail.com>
2019-02-05 20:22:12 -0600

    Added test class and Autonomous Commaon class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousCommon.java
@@ -0,0 +1,40 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import java.util.List;
+
+public class AutonomousCommon  {
+    public static void ChrisTest(DcMotor leftMotor, DcMotor rightMotor){
+
+        leftMotor.setPower(-1);        //go backward
+        rightMotor.setPower(-1);
+        sleep(3000);
+        StopMotors(new DcMotor[]{leftMotor,rightMotor});
+
+        rightMotor.setPower(1);
+        sleep(2000);
+        StopMotors(new DcMotor[]{leftMotor,rightMotor});
+
+        leftMotor.setPower(1);        //go backward
+        rightMotor.setPower(1);
+        StopMotors(new DcMotor[]{leftMotor,rightMotor});
+    }
+
+
+    public static void StopMotors(DcMotor[] motors){
+        for (DcMotor motor : motors ) {
+            motor.setPower(0);
+        }
+    }
+    //sleep from LinearOpMode class
+    public static void sleep(long milliseconds) {
+        try {
+            Thread.sleep(milliseconds);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SimpleRobotTest.java
@@ -0,0 +1,38 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+// below is the Annotation that registers this OpMode with the FtcRobotController app.
+// @Autonomous classifies the OpMode as autonomous, name is the OpMode title and the
+// optional group places the OpMode into the Exercises group.
+// uncomment the @Disable annotation to remove the OpMode from the OpMode list.
+@Autonomous(name = "SimpleRobotTest", group = "Exercises")
+//@Disabled
+public class SimpleRobotTest extends LinearOpMode {
+    private DcMotor rightMotor;
+    private DcMotor leftMotor;
+
+    // called when init button is  pressed.
+    @Override
+    public void runOpMode() {
+        leftMotor = hardwareMap.dcMotor.get("leftMotor");
+        rightMotor = hardwareMap.dcMotor.get("rightMotor");
+
+        leftMotor.setDirection(DcMotor.Direction.FORWARD);
+        rightMotor.setDirection(DcMotor.Direction.REVERSE);
+
+
+        telemetry.addData("Mode", "waiting");
+        telemetry.update();
+        // wait for start button.
+        waitForStart();
+        telemetry.addData("Mode", "running");
+        telemetry.update();
+
+        AutonomousCommon.ChrisTest(leftMotor,rightMotor);
+    }
+}
+
