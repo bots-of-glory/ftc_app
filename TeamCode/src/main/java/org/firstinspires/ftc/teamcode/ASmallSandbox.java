75fd1108e31522e463c5b1b3b7d03bc53f2ed0f5
alexander-smawesome <51275303+alexander-smawesome@users.noreply.github.com>
2019-08-23 20:15:20 -0500

    Testing moving forward and straifing (forgive me if I spelled that wrong)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ASmallSandbox.java
@@ -0,0 +1,73 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cGyro;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.GyroSensor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.vuforia.Vuforia;
+
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.teamcode.vision.MasterVision;
+import org.firstinspires.ftc.teamcode.vision.SampleRandomizedPositions;
+
+@Autonomous (name="VuDepotSide", group="Competition Autonomous")
+public class VuDepotSide extends LinearOpMode{
+    MasterVision vision;
+    SampleRandomizedPositions goldPosition;
+    DcMotor liftMotor;
+    DcMotor frontLeft;
+    DcMotor rearLeft;
+    DcMotor frontRight;
+    DcMotor rearRight;
+    Servo armServo;
+    GyroSensor gsensorGyro;
+    ModernRoboticsI2cGyro mrGyro;
+
+    private ElapsedTime runtime = new ElapsedTime();
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        //declare motors
+        liftMotor = hardwareMap.dcMotor.get("liftMotor");
+        frontLeft = hardwareMap.dcMotor.get("frontLeft");
+        rearLeft = hardwareMap.dcMotor.get("rearLeft");
+        frontRight = hardwareMap.dcMotor.get("frontRight");
+        rearRight = hardwareMap.dcMotor.get("rearRight");
+        sensorGyro = hardwareMap.gyroSensor.get("gyro");
+        armServo = hardwareMap.servo.get("armServo");
+
+        //declare motor directions
+        frontLeft.setDirection(DcMotor.Direction.FORWARD);
+        rearLeft.setDirection(DcMotor.Direction.FORWARD);
+        frontRight.setDirection(DcMotor.Direction.REVERSE);
+        rearRight.setDirection(DcMotor.Direction.REVERSE);
+        liftMotor.setDirection(DcMotor.Direction.REVERSE);
+        armServo.setDirection(Servo.Direction.FORWARD);
+
+        mrGyro = (ModernRoboticsI2cGyro) sensorGyro;
+
+
+        double turnSpeed = 0.2;
+        int target = 0;
+
+        frontLeft.setPower(1);
+        rearLeft.setPower(-1);
+        frontRight.setPower(1);
+        rearRight.setPower(-1);
+        Thread.sleep(5000);
+        //Move forward
+        frontLeft.setPower(1);
+        rearLeft.setPower(1);
+        frontRight(-1);
+        frontLeft(1);
+        Thread.sleep 200;
+        //move left
+    }}}
