de6e6dd72c5eec9c77d7c80277aea7298e77cfce
Jacob Hettinger <jacob.hettinger1@gmail.com>
2019-09-17 18:43:56 -0500

    Update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BuildingZoneBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BuildingZoneBlue.java
@@ -1,4 +1,53 @@
 package org.firstinspires.ftc.teamcode;
 
-public class BuildingZoneBlue {
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cGyro;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.GyroSensor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.vision.MasterVision;
+import org.firstinspires.ftc.teamcode.vision.SampleRandomizedPositions;
+
+@Autonomous (name="BuildingZoneBlue", group="Competition Autonomous")
+public class BuildingZoneBlue extends LinearOpMode {
+    MasterVision vision;
+    SampleRandomizedPositions goldPosition;
+    DcMotor liftMotor;
+    DcMotor frontLeft;
+    DcMotor rearLeft;
+    DcMotor frontRight;
+    DcMotor rearRight;
+    Servo armServo;
+    GyroSensor sensorGyro;
+    ModernRoboticsI2cGyro mrGyro;
+
+    private ElapsedTime runtime = new ElapsedTime();
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        //declare motors
+        liftMotor = hardwareMap.dcMotor.get("liftMotor");
+        frontLeft = hardwareMap.dcMotor.get("frontLeft");
+        rearLeft = hardwareMap.dcMotor.get("rearLeft");
+        frontRight = hardwareMap.dcMotor.get("frontRight");
+        rearRight = hardwareMap.dcMotor.get("rearRight");
+        sensorGyro = hardwareMap.gyroSensor.get("gyro");
+        armServo = hardwareMap.servo.get("armServo");
+
+        //declare motor directions
+        frontLeft.setDirection(DcMotor.Direction.FORWARD);
+        rearLeft.setDirection(DcMotor.Direction.FORWARD);
+        frontRight.setDirection(DcMotor.Direction.REVERSE);
+        rearRight.setDirection(DcMotor.Direction.REVERSE);
+        liftMotor.setDirection(DcMotor.Direction.REVERSE);
+        armServo.setDirection(Servo.Direction.FORWARD);
+
+        mrGyro = (ModernRoboticsI2cGyro) sensorGyro;
+
+    }
 }
